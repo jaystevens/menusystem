e609f750fe79a2f74d0e607bd1e0ca8f1305089d
Jason Stevens <jay@wizardofthenet.com>
2019-12-21 23:54:28 -0800

    fix bad import after upgrading to python3

--- menusystem/XMLMenuGenie.py
+++ menusystem/XMLMenuGenie.py
@@ -16,7 +16,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 """
 import sys, xml.dom.minidom
-from . import menusystem
+import menusystem
 """Menu System
 
    Author:  Daniel Mikusa <dan@trz.cc>
